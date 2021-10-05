8ac61e5d9a1408fac88ec54b05b95ae0dbd472c2
Michael Walker <walkerrepair255@yahoo.com>
2021-10-05 17:55:03 -0400

    Add project files.

---
+++ EF-Tutorial/Program.cs
@@ -0,0 +1,31 @@
+﻿using EF_Tutorial.Models;
+using System;
+using System.Linq;
+
+namespace EF_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _context = new EdDbContext();
+            Console.WriteLine("Major table prac");
+            var majors = _context.Majors
+                .Where(m => m.Code.Contains("M"))        
+                
+                .ToList();
+            foreach (var m in majors)
+            {
+                Console.WriteLine($"{m.Code}");
+            }
+            Console.WriteLine("Student table prac");
+            var students = _context.Students
+                .Where(s => s.Gpa > 3)
+                .ToList();
+            foreach (var stud in students)
+            {
+                Console.WriteLine($"{stud.Firstname} {stud.Lastname} | {stud.Gpa}");
+            }
+        }
+    }
+}
